7e11a4c47d0783730c5a0fe0d66dd04e5a8662f3
Chris foley <chris.w.foley1980@gmail.com>
2020-07-19 21:53:23 -0500

    FK_PK complete, first query complete

--- schema.sql
+++ schema.sql
@@ -8,14 +8,14 @@ CREATE TABLE departments (
      UNIQUE (dept_name)
 );
 
-CREATE TABLE employees ( 
-	emp_no INT NOT NULL, 
-	birth_date DATE NOT NULL,
-	first_name VARCHAR NOT NULL,
-	last_name VARCHAR NOT NULL,
-	gender VARCHAR NOT NULL,
-	hire_date DATE NOT NULL,
-	PRIMARY KEY (emp_no)
+CREATE TABLE employees (
+	 emp_no INT NOT NULL,
+     birth_date DATE NOT NULL,
+     first_name VARCHAR NOT NULL,
+     last_name VARCHAR NOT NULL,
+     gender VARCHAR NOT NULL,
+     hire_date DATE NOT NULL,
+     PRIMARY KEY (emp_no)
 );
 
 CREATE TABLE dept_manager (
@@ -38,21 +38,20 @@ CREATE TABLE salaries (
 );
 
 CREATE TABLE titles (
-	emp_no INT NOT NULL, 
-	title VARCHAR (20) NOT NULL,
-	from_date DATE NOT NULL,
-	to_date DATE NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (emp_no)
+  emp_no INT NOT NULL,
+  title VARCHAR NOT NULL,
+  from_date DATE NOT NULL,
+  to_date DATE NOT NULL,
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+  PRIMARY KEY (emp_no)
 );
 
-CREATE TABLE dep_emp (
+CREATE TABLE dept_emp (
 	emp_no INT NOT NULL,
-	dept_no INT NOT NULL,
+	dept_no VARCHAR(4) NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (emp_no)
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+	PRIMARY KEY (emp_no, dept_no)
 );
-
-SELECT * FROM departments;
